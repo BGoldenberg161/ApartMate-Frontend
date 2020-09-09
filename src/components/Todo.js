c84a80facb0415b02c614409e01c65498d0b8609
yoel <yoel@macbook-pro.home>
2020-09-09 15:29:47 -0700

    [fix] taskName Displays (chore List)

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -22,14 +22,14 @@ const Todo = ({ todos, completeTodo, removeTodo, updateTodo, props }) => {
   if (edit.id) {
     return <TodoForm edit={edit} onSubmit={submitUpdate} user={props.user} />;
   }
-
+console.log(todos)
   return todos.map((todo, index) => (
     <div
       className={todo.isComplete ? 'todo-row complete' : 'todo-row'}
       key={index}
     >
       <div key={todo.id} onClick={() => completeTodo(todo.id)}>
-        {todo.text}
+        {todo.taskName}
       </div>
       <div className='icons'>
         <RiDeleteBin5Line
@@ -37,7 +37,7 @@ const Todo = ({ todos, completeTodo, removeTodo, updateTodo, props }) => {
           className='delete-icon'
         />
         <TiEdit
-          onClick={() => setEdit({ id: todo.id, value: todo.text })}
+          onClick={() => setEdit({ id: todo.id, value: todo.taskName })}
           className='edit-icon'
         />
       </div>
--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,75 +1,82 @@
 import React, { useState, useEffect, useRef } from 'react';
-import axios from 'axios'
-const REACT_APP_SERVER_URL = process.env.REACT_APP_SERVER_URL
-
+import axios from 'axios';
+const REACT_APP_SERVER_URL = process.env.REACT_APP_SERVER_URL;
 
 function TodoForm(props) {
-  const [input, setInput] = useState(props.edit ? props.edit.value : '');
-  const thisUser = props.user
-  const [taskDetail, setTaskDetail] = useState('No wammies, no wammies, no wammies')
-  const [group, setGroup] = useState('5f555fdfc294e44af3aca930')
-  const [rep, isRep] = useState(true)
+	const [input, setInput] = useState(props.edit ? props.edit.value : '');
+	const thisUser = props.user;
+	const [taskDetail, setTaskDetail] = useState(
+		'No wammies, no wammies, no wammies'
+	);
+	const group = props.groupId;
+	const [rep, isRep] = useState(true);
 
-  const inputRef = useRef(null);
+	const inputRef = useRef(null);
 
-  useEffect(() => {
-    inputRef.current.focus();
-  });
+	useEffect(() => {
+		inputRef.current.focus();
+	});
 
-  const handleChange = e => {
-    setInput(e.target.value);
-  };
+	const handleChange = e => {
+		setInput(e.target.value);
+	};
 
-  const handleSubmit = e => {
-    e.preventDefault();
+	const handleSubmit = e => {
+		e.preventDefault();
 
-      axios.post(`${REACT_APP_SERVER_URL}/chores/new`, {input, thisUser, taskDetail, group, rep})
-        .then(newChore => {
-          console.log(newChore)
-        })
-        .catch(err => console.log(`Post chore error:`, err))
+		axios
+			.post(`${REACT_APP_SERVER_URL}/chores/new`, {
+				input,
+				thisUser,
+				taskDetail,
+				group,
+				rep,
+			})
+			.then(newChore => {
+				console.log(newChore);
+			})
+			.catch(err => console.log(`Post chore error:`, err));
 
-    props.onSubmit({
-      id: Math.floor(Math.random() * 10000),
-      text: input
-    });
-    setInput('');
+		props.onSubmit({
+			id: Math.floor(Math.random() * 10000),
+			taskName: input,
+		});
+		setInput('');
+	};
 
-  };
-
-  return (
-    <form onSubmit={handleSubmit} className='todo-form'>
-      {props.edit ? (
-        <>
-          <input
-            placeholder='Update your item'
-            value={input}
-            onChange={handleChange}
-            name='text'
-            ref={inputRef}
-            className='todo-input edit'
-          />
-          <button onClick={handleSubmit} className='todo-button edit'>
-            Update
-          </button>
-        </>
-      ) : (
-        <>
-          <input
-            placeholder='Add a todo'
-            value={input}
-            onChange={handleChange}
-            name='text'
-            className='todo-input'
-            ref={inputRef}
-          />
-          <button onClick={handleSubmit} className='todo-button'>
-            Add todo
-          </button>
-        </>
-      )}
-    </form>
-  );
+	return (
+		<form onSubmit={handleSubmit} className='todo-form'>
+			{props.edit ? (
+				<>
+					<input
+						placeholder='Update your item'
+						value={input}
+						onChange={handleChange}
+						name='taskName'
+						ref={inputRef}
+						className='todo-input edit'
+					/>
+					<button onClick={handleSubmit} className='todo-button edit'>
+						Update
+					</button>
+				</>
+			) : (
+				<>
+					<input
+						placeholder='Add a todo'
+						value={input}
+						onChange={handleChange}
+						name='taskName'
+						className='todo-input'
+						ref={inputRef}
+					/>
+					<button onClick={handleSubmit} className='todo-button'>
+						Add todo
+					</button>
+				</>
+			)}
+		</form>
+	);
 }
 
 export default TodoForm;
